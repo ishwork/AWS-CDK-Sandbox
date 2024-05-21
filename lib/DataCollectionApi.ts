4d65967f1dd6a953157c2466453763ac67bacc18
ishwork <ishworkhadka70@gmail.com>
2024-05-22 00:55:53 +0300

    update: requestTemplates in DataCollectionApi stack

--- lib/DataCollectionApi.ts
+++ lib/DataCollectionApi.ts
@@ -111,7 +111,7 @@ export class DataCollectionApi extends Stack {
             "Records": [
               #foreach($elem in $input.path('$'))
               {
-                "Data": "$util.base64Encode($input.json('$'))",
+                "Data": "$util.base64Encode($elem.data)",
                 "PartitionKey": "$elem.category"
               }#if($foreach.hasNext),#end
               #end
